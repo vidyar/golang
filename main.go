81914fc3c1634873b7233c92281191a26e16ba21
ZhiFeng Hu <hufeng1987@gmail.com>
2015-05-22 18:35:21 +0800

    重新定义控制器

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
+func pingCtr(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+	r.GET("/ping", pingCtr)
 
 	// Listen and serve on 0.0.0.0:8080
 	r.Run(":8080")
