45b10699c28196a314d39afe0d7d344f378cf619
ZhiFeng Hu <hufeng1987@gmail.com>
2015-05-22 18:38:24 +0800

    add home controll

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 func pingCtr(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
+func homeCtr(c *gin.Context) {
+	c.String(http.StatusOK, "home control")
+}
 
 func main() {
 	r := gin.Default()
+	r.GET("/", homeCtr)
 	r.GET("/ping", pingCtr)
 
 	// Listen and serve on 0.0.0.0:8080
