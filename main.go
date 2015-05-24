6e41865664dec85eb746ab1d65c6e4240b7a7584
ZhiFeng Hu <hufeng1987@gmail.com>
2015-05-24 11:06:29 +0800

    Login progress

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 func pingCtr(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
 func homeCtr(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
+func loginCtr(c *gin.Context) {
+	c.HTML(http.StatusOK, "admin-login.html", gin.H{})
+}
+func loginProcessCtr(c *gin.Context) {
+	var form LoginForm
+	c.BindWith(&form, binding.MultipartForm)
+
+	if form.Username == "netroby" && form.Password == "deyilife" {
+		c.String(http.StatusOK, "you are logged in")
+	} else {
+		c.String(http.StatusOK, "Login failed" + form.Username)
+	}
+}
+type LoginForm struct {
+	Username     string `form:"username" binding:"required"`
+	Password string `form:"password" binding:"required"`
 }
 
 func main() {
@@ -18,6 +36,11 @@ func main() {
 	r.GET("/", homeCtr)
 	r.GET("/ping", pingCtr)
 
+	v1 := r.Group("/admin")
+	{
+		v1.GET("/login", loginCtr)
+		v1.POST("/login-process", loginProcessCtr)
+	}
 	// Listen and serve on 0.0.0.0:8080
 	r.Run(":8080")
 }
