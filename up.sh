68e3a6caa53f7e3c6555fff61e73e425ea2a6c16
ZhiFeng Hu <hufeng1987@gmail.com>
2015-10-28 21:20:09 +0800

    Update up.sh
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- up.sh
+++ up.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+go get github.com/tools/godep
+godep go build
 docker rm -vf gosense
 # docker run --restart=always -p 80:8080 --link db:db -v /data/www/gosense:/www --name gosense netroby/alpgo /www/gosense
-docker run --restart=always -d --name db netroby/docker-mysql
+docker run --restart=always -d --name db  -v /mysql-data netroby/docker-mysql
 sleep 15
 docker cp sql/bak.sql db:/root/
 docker exec db sh -c "mysql < /root/bak.sql"
-docker run --restart=always -d --link db:db -v /data/www/gosense:/www --name gosense netroby/alpgo /www/gosense
+docker run --restart=always -d --link db:db -v $(pwd):/www --name gosense netroby/alpgo /www/gosense
